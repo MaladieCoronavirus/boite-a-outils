635bfaa4bcad95ad0e93305e7da2e84c35c8cf68
natalia chernova <n.chernova@meetic-corp.com>
2020-03-31 12:10:40 +0200

    Add footer links and text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,11 +5,21 @@ import { Link } from 'gatsby';
 import { colors } from 'Style/colors';
 
 const FooterContainer = styled.footer`
-  width: 100%;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  justify-content: space-between;
   align-self: end;
-  background: ${colors.bgMuted};
+  background: ${colors.bgHighlight};
   min-height: 6rem;
-  padding: 1.45rem 1.0875rem;
+  padding: 1.5rem 1.5rem;
+  grid-column-gap: 4rem;
+`;
+
+const FooterHeadline = styled.h3`
+  font-size: 1.1rem;
+  font-weight: bold;
+  color: ${colors.primary};
+  margin-bottom: 1.2rem;
 `;
 
 const List = styled.ul`
@@ -17,27 +27,92 @@ const List = styled.ul`
   list-style: none;
 `;
 
-const ListItem = styled.li`
-  a {
-    text-decoration: none;
-    color: ${colors.text};
-    font-size: .9rem;
+const TextContainer = styled.p`
+  margin-top: 2.3rem;
+  font-size: 1rem;
+  line-height: 1.5rem;
+`;
 
+const LinkContainer = styled.span`
+  a {
+    color: ${props => props.secondary ? colors.secondary : colors.text};
+    text-decoration: none;
+    font-size: 1rem;
+    line-height: 1.5rem;
+  
     &:hover {
-      color: ${colors.text};
+      color: ${props => props.secondary ? colors.secondary : colors.text};
+      text-decoration: underline;
     }
   }
 `;
 
 const Footer = () => (
     <FooterContainer>
-      <List>
-        <ListItem>
-          <Link to="/mentions-legales">
-            Mentions légales
-          </Link>
-        </ListItem>
-      </List>
+      <section>
+        <FooterHeadline>
+          Contacts utiles
+        </FooterHeadline>
+        <List>
+          <li>
+            <LinkContainer>
+              <a href="tel:0800130000">
+                0 800 130 000 - (appel gratuit)
+              </a>
+            </LinkContainer>
+          </li>
+          <li>
+            <LinkContainer secondary>
+              <a href="https://www.gouvernement.fr/info-coronavirus" target="_blank">
+                gouvernement.fr/info-coronavirus
+              </a>
+            </LinkContainer>
+          </li>
+        </List>
+      </section>
+      <section>
+        <FooterHeadline>
+          Navigation
+        </FooterHeadline>
+        <List>
+          <li>
+            <LinkContainer>
+              <Link to="/">
+                Accueil
+              </Link>
+            </LinkContainer>
+          </li>
+          <li>
+            <LinkContainer>
+              <Link to="/mentions-legales">
+                Mentions légales
+              </Link>
+            </LinkContainer>
+          </li>
+          <li>
+            <LinkContainer>
+              <Link to="/partenaires">
+                Partenaires
+              </Link>
+            </LinkContainer>
+          </li>
+          <li>
+            <LinkContainer>
+              <Link to="/presse">
+                Presse
+              </Link>
+            </LinkContainer>
+          </li>
+        </List>
+      </section>
+      <section>
+        <TextContainer>
+         Algorithme d'orientation mis à jour sur les recommandations en vigueur au 22/03/2020 à 16h15.
+          Ce site d’information n’est pas un dispositif médical, il ne délivre pas d’avis médical.
+          Projet solidaire d'urgence, financé par <LinkContainer secondary><Link to="/partenaires">l’Alliance Digitale contre le COVID-19 </Link> </LinkContainer>
+           : <LinkContainer secondary><a href="https://twitter.com/maladiecovid19" target="_blank">@maladiecovid19</a></LinkContainer>
+        </TextContainer>
+      </section>
     </FooterContainer>
 );
 
