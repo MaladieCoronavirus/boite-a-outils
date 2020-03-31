cd94e0999abc108ccfff56d8c2d460038992191a
NatChe <chernova.natalia@yahoo.fr>
2020-03-31 17:05:11 +0200

    Add footer links and text (#2)
    Co-authored-by: natalia chernova <n.chernova@meetic-corp.com>

--- src/style/colors.js
+++ src/style/colors.js
@@ -1,9 +1,10 @@
 export const colors = {
-  primary: '#0064D5',
+  primary: '#0069CC',
+  secondary: '#FF8000',
   accent: '#FF8000',
   text: '#3B3B3B',
   bg: '#F4F4F4',
-  bgHighlight: '#FFFFFF',
+  bgHighlight: '#F9F9F9',
   bgPrimary: '#0069CC',
   bgMuted: '#F0F0F0',
 }
