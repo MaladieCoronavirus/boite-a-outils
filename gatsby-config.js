60686feecd3ed06dc3fd139da212c528907f5e1b
Julien Passeri <j.passeri@meetic-corp.com>
2020-03-31 19:15:27 +0200

    feat: remove gatsby-plugin-web-font-loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,14 +19,6 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Inter: 400, 500, 700']
-        }
-      }
-    },
-    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: { default: path.resolve('./src/components/Layout') },
