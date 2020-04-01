e4db0a023244c44877f7f1a35a43b41f0da9e256
NatChe <chernova.natalia@yahoo.fr>
2020-04-01 16:12:38 +0200

    Add target cards as main navigation (#8)
    Co-authored-by: natalia chernova <n.chernova@meetic-corp.com>

---
+++ src/components/TargetCards/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import TargetCard from 'Components/TargetCard';
+
+const CardsContainer = styled.div`
+  display: grid;
+  gap: 2rem;
+  padding: 2rem 0;
+  list-style-type: none;
+
+  @media (min-width: 30rem) {
+    grid-template-columns: repeat(auto-fit, minmax(26rem, 1fr));
+  }
+`;
+
+const TargetCards = () => {
+  return (
+      <CardsContainer>
+        <TargetCard title="Un patient" link="#patient"/>
+        <TargetCard title="Un praticien" link="#praticien"/>
+        <TargetCard title="Un établissement" link="#etablissement"/>
+      </CardsContainer>
+  );
+}
+
+export default TargetCards;
