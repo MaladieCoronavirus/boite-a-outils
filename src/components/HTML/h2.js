f5949649b7692287a28bd68c2fc90faf6ff16104
Simone Mauro <simone@simonemauro.com>
2020-04-01 15:04:49 +0200

    Change heading elements used in sections

--- src/components/HTML/h2.js
+++ src/components/HTML/h2.js
@@ -1,14 +1,5 @@
-import styled from '@emotion/styled';
+import H1 from './h1';
 
-import { colors } from 'Style/colors';
-
-const H2 = styled.h2`
-  margin-bottom: 0.5rem;
-  font-size: 1.75rem;
-  line-height: 1.21428;
-  font-weight: 500;
-  color: ${colors.accent};
-  text-align: center;
-`
+const H2 = H1.withComponent('h2');
 
 export default H2;
---
+++ src/components/HTML/h3.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+import { colors } from 'Style/colors';
+
+const H3 = styled.h3`
+  margin-bottom: 0.5rem;
+  font-size: 1.75rem;
+  line-height: 1.21428;
+  font-weight: 500;
+  color: ${colors.accent};
+  text-align: center;
+`
+
+export default H3;
